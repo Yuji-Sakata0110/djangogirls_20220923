0a245b1d6d8474ddedea474a83b58bfa410aa71d
yujis <yuji.sakata0110@gmail.com>
2022-09-25 18:02:44 +0900

    fix pk auto increatse and make draft_list

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
+    path('post/new/draftsave/', views.post_new_draft_save, name='post_new_draft_save'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/edit/draftsave/',views.post_edit_draft_save, name='post_edit_draft_save'),
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
 ]
