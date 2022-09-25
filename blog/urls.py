a4e474656ff3cc158dbe59f15da057694a2aa4b5
yujis <yuji.sakata0110@gmail.com>
2022-09-25 18:07:43 +0900

    clean urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,9 +6,8 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/new/draftsave/', views.post_new_draft_save, name='post_new_draft_save'),
-    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<int:pk>/edit/draftsave/',views.post_edit_draft_save, name='post_edit_draft_save'),
-    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
 ]
