29269a1a229fba38482b3e04337e0ee947c37446
yujis <yuji.sakata0110@gmail.com>
2022-10-12 21:48:31 +0900

    server error output

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+handler500 = views.my_customized_server_error
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
--- blog/views.py
+++ blog/views.py
@@ -105,3 +105,14 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request, 'blog/comment.html', {'form': form, 'pk': pk})
+
+# error handler
+from django.views.decorators.csrf import requires_csrf_token
+from django.http import HttpResponseServerError
+
+@requires_csrf_token
+def my_customized_server_error(request, template_name='500.html'):
+    import sys
+    from django.views import debug
+    error_html = debug.technical_500_response(request, *sys.exc_info()).content
+    return HttpResponseServerError(error_html)
